12513ba006ebec4cc44e06cad1bf0a7f80602b28
Jonathan Martin <me@nybblr.com>
2014-04-24 10:29:30 -0400

    Register emblem as its own.

--- lib/emblem/rails/engine.rb
+++ lib/emblem/rails/engine.rb
@@ -3,7 +3,7 @@ require 'emblem/rails/template'
 module Emblem
   module Rails
     class Engine < ::Rails::Engine
-      initializer "ember_rails.setup", :after => :append_assets_path, :group => :all do |app|
+      initializer "emblem_rails.register", :after => :append_assets_path, :group => :all do |app|
         app.assets.register_engine '.emblem', Emblem::Rails::Template
       end
     end
